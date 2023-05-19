44eda6d06f1cd77dd84d060c6b9dd7062f46d933
Viuleeenz <Viuleeenz@users.noreply.github.com>
2023-05-19 15:47:32 +0200

    Update targets.js
    code adjustment

--- DynamicBinaryInstrumentation/DDosia_Targets/targets.js
+++ DynamicBinaryInstrumentation/DDosia_Targets/targets.js
@@ -2,34 +2,6 @@ var ptr_WSAStartup = Module.getExportByName(null,"WSAStartup");
 var counter = 0;
 var PROTECTION = 'wr-';
 
-function writeBufferToFile(filename, buffer){
-	let file = new File(filename, 'wb');
-	file.write(buffer);
-	file.flush();
-	file.close();
-}
-
-function dumpProcessMemory(protection) {
-	let ranges = Process.enumerateRanges(protection);
-	console.log('[BEGIN] Memory ranges located: ' + ranges.length );
-	ranges.forEach(function (range) {
-		let destFileName = `dumps/${range.base}_dump`;
-		let arrayBuffer = null;
-
-		try {
-			arrayBuffer = range.base.readByteArray(range.size);
-		}
-		catch(e){
-			console.log('[ERROR] Dumping memory at: ' + range.base);
-		}
-
-		if (arrayBuffer){
-			writeBufferToFile(destFileName,arrayBuffer);
-		}
-	});
-
-}
-
 Interceptor.attach(ptr_WSAStartup,
 {
 	onEnter: function (args)
